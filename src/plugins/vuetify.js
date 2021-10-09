f0367e20f074423cd9b057ae6306f311429bad99
Dang Nguyen <nvdang@tma.com.vn>
2021-10-09 12:22:58 +0700

    Init library

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import { VBtn } from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+    components: {
+        VBtn
+    }
+});
+
+export default new Vuetify({
+    
+});
